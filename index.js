6be9640d4493c62e89e8d38ee0e60228690db6c7
Viktor Fišer <34626149+vikimiki11@users.noreply.github.com>
2021-01-23 12:22:58 +0100

    backup before change in mongoldb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+gridsize = [7, 11]//[x,y] Tell how big is gaming grid
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -6,76 +7,176 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
 const MongoClient = require('mongodb').MongoClient;
-const { resolve } = require('path');
 const url = require('fs').readFileSync('url.txt', 'utf8');
-var createUser
+var serverlist = {}
 server.listen(port, () => {
 	console.log('Server listening at port %d', port);
 });
-
+var membersact = { "": { name: "", room: false } }
 app.use(express.static(path.join(__dirname, 'public')));
 
-logs=[]
-function logit(mes){
+logs = []
+function logit(mes) {
 	var d = new Date();
-	mes=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+":"+d.getMilliseconds()+"<br>"+mes
-	io.to('log').emit("nlog",mes)
+	mes = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMilliseconds() + "<br>" + mes
+	io.to('log').emit("nlog", mes)
 	console.log(mes)
-	logs[logs.length]=mes
+	logs[logs.length] = mes
 }
-//io.emit('',data)
-//socket.emit('',data);
+
+
+
+
+
+
+
 // MARK: Comunication
 io.on('connection', (socket) => {
-	socket.on('signup', async(data) => {
+	socket.on('signup', async (data) => {
 		temp = await CreateUser(data[0], data[1])
-		console.log("backsignup"+temp)
-		socket.emit("backsignup",temp)
+		console.log("backsignup" + temp)
+		socket.emit("backsignup", temp)
 	});
-	socket.on('login', async(data) => {
-		if (await login(data[0],data[1])) {
-			socket.emit('backlogin', true)
-			console.log("backlogin true")
+	socket.on('loginin', async (data) => {
+		if (await login(data[0], data[1])) {
+			socket.userdata = await FindUser(data[0])
+			console.log("login")
+			console.log(socket.userdata)
+			membersact[data[0]] = { name: data[0], room: false }
+			io.emit('players', membersact)
+			socket.emit('backlogin', socket.userdata)
 		} else {
 			socket.emit('backlogin', false)
-			console.log("backlogin false")
 		}
 	})
 	socket.on('disconnect', () => {
+		if (socket.userdata) {
+			console.log("disconect")
+			console.log(socket.userdata)
+			temp = removeFromObject(membersact, socket.userdata.name)
+			if (temp == membersact) console.warn("no change in membersact after disconect")
+			membersact = temp
+			io.emit('players', membersact);
+			console.log(membersact)
+		} else {
+			console.log("some ... didnt login and just quit")
+		}
 	});
+	socket.on('new message', data => {
+		if (data[1] == "g") {
+			console.log("G Mes" + JSON.stringify(data))
+			socket.broadcast.emit('new message', data[0])
+		} else {
+			console.log(data[1]+" Mes:" + JSON.stringify(data))
+			socket.broadcast.to(data[1]).emit('new message', data[0])
+		}
+	});
+	socket.on('creategame', data => {
+		socket.join(socket.userdata.name)
+		membersact[socket.userdata.name].active = 1;
+		membersact[socket.userdata.name].room = socket.userdata.name;
+		if (data[0]) {
+			serverlist[socket.userdata.name] = { name: socket.userdata.name, score: socket.userdata.score, active:1 }
+		} else {
+			console.log("//TODO: friend server")
+		}
+		console.log("server created: " + socket.userdata.name)
+		io.emit('serverlist', serverlist)
+	})
+	socket.on('joingame', data => {
+		if (serverlist[data[0]].active==1 && data[0] != socket.userdata.name && membersact[data[0]].active) {
+			socket.join(data[0]);
+			serverlist[data[0]].active = 2;
+			membersact[socket.userdata.name].room = data[0];
+			membersact[socket.userdata.name].active = 1;
+			membersact[socket.userdata.name].room = data[0]
+			console.log(socket.userdata.name + " joined to game with " + data[0]);
+			removeFromObject(serverlist, data[0]);
+			start_of_game(data[0], socket.userdata.name);
+		}
+	})
+	socket.on('baseplaced', data => { baseplaced(data) })
 })
 
+
+
+
+
+
+
 // MARK: Database functions
-async function CreateUser(name,pass){
-	if(!await FindUser(name)){
+async function CreateUser(name, pass) {
+	if (!await FindUser(name)) {
 		return new Promise(resolve => {
-			MongoClient.connect(url, function(err, db) {
+			MongoClient.connect(url, function (err, db) {
 				if (err) throw err;
-				db.db("ToDeMu").collection("users").insertOne({ name: name, password: pass }, function(err, res) {
+				db.db("ToDeMu").collection("users").insertOne({ name: name, password: pass, friends: ["viki", "f"], startedgames: 0, win: 0, lost: 0, buildtowers: 0, buildtroopers: 0, score: 1000 }, function (err, res) {
 					if (err) throw err;
-					if(res.result.ok) logit("was created user "+name);
+					if (res.result.ok) logit("was created user " + name);
 					resolve(Boolean(res.result.ok));
 				});
 			});
 		});
-	}else return false;
+	} else return false;
 }
-function FindUser(name){
+function FindUser(name) {
 	return new Promise(resolve => {
-		MongoClient.connect(url, function(err, db) {
+		MongoClient.connect(url, function (err, db) {
 			if (err) throw err;
-			db.db("ToDeMu").collection("users").findOne({name:name}, function(err, result) {
+			db.db("ToDeMu").collection("users").findOne({ name: name }, function (err, result) {
 				if (err) throw err;
 				resolve(result);
 			});
 		});
 	});
 };
-function login(name,pass){
-	return new Promise(async resolve => {
-		x = await FindUser(name)
-		x=x?x:"none"
-		logit(x.password == pass)
-		resolve(x.password == pass)
-	})
+function login(name, pass) {
+	if (!membersact[name]) {
+		return new Promise(async resolve => {
+			x = await FindUser(name)
+			x = x ? x : resolve(false)
+			resolve(x.password == pass)
+		})
+	}
+}
+
+
+
+
+
+
+
+// MARK: Usefull functions
+function removeFromArray(arr, el) {
+	return arr.filter(function (x) {
+		return x != el;
+	});
+}
+function removeFromObject(arr, el) {
+	i = 0
+	ret = {}
+	for (x in arr) {
+		if (x != el) {
+			ret[x] = arr[x]
+		}
+	}
+	arr = ret
+	return ret
+}
+
+
+
+
+
+
+
+// TODO: DO the FuCKInG GaMe lol 
+function start_of_game(us1, us2) {
+	io.to(us1).emit("placebase", [us1, us2, gridsize]);
+}
+function baseplaced(data) {
+
+}
+function game_tick(gamedata) {
+
 }
--- public/js.js
+++ public/js.js
@@ -1,11 +1,63 @@
-connected = false
-username = false
-spam = false
+connected = false;
+serverlist = {};
+playerdata = {};
+Name = false;
+spam = false;
+zpravy = { g: [] };
+actualchat = "g";
+temp = document.querySelectorAll(".chat");
+DOMchatg = temp[0];
+DOMchatl = temp[1];
+ingame = false;
+var COLORS = ['0F0', '0EE', '22F', 'EE0', 'E00', 'E0E'];
+if (localStorage.getItem('name')) {
+	document.querySelector(".usernameInput").value = localStorage.getItem('name');// TODO: localStorage.setItem('name', username);
+}
+var membersact = {}
 if (window.location.hostname == "localhost") {
 	socket = io("localhost:8080");
 } else {
 	socket = io(window.location.hostname);
 }
+function switchchat() {
+	if (actualchat == "g" && ingame) {// TODO: switch to local only in game
+		actualchat = "l"
+		DOMchatl.style.display = "block"
+		DOMchatg.style.display = "none"
+	}
+	else {
+		actualchat = "g"
+		DOMchatg.style.display = "block"
+		DOMchatl.style.display = "none"
+	}
+}
+socket.on('new message', (data) => {
+	addChatMessage({ username: data.username, message: data.message, skupina: "L" });
+});
+const addChatMessage = (data) => {
+	el = document.querySelector(".Chat" + data.skupina.toUpperCase())
+	el.innerHTML += '<p class="message" style="display: block;"><span class="username" style="color: #' + getUsernameColor(data.username) + ';">' + data.username + '</span><span class="messageBody">' + data.message + '</span></p>'
+}
+const sendMessage = (mes) => {
+	if (mes) {
+		temp = actualchat == "g" ? "g" : membersact[Name].room
+		temp2 = {
+			username: Name,
+			message: mes,
+			skupina: actualchat
+		}
+		addChatMessage(temp2);
+		socket.emit('new message', [temp2, temp]);
+	}
+}
+
+
+
+
+
+
+
+// MARK: usefull functions
 async function sha256(message) {
 	const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
 	const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
@@ -13,88 +65,82 @@ async function sha256(message) {
 	const hashHex = hashArray.map(b => b.toString(36).padStart(2, '0')).join(''); // convert bytes to hex string
 	return hashHex;
 }
-function switchchat() {
-	sa = document.querySelector("#rs").className
-	if (sa == "SwitchG") {
-		if (membersact[username].room !== "") {
-			document.querySelector("#rs").className = "SwitchL"; document.querySelector("#rs").innerHTML = "Local chat"
-		}
-	} else {
-		document.querySelector("#rs").className = "SwitchG"; document.querySelector("#rs").innerHTML = "Global chat"
-	}
-	if (document.querySelector("#rs").className != sa) {
-		document.querySelector(".chat").innerHTML = ""
-		rs = document.querySelector("#rs").className.split("Switch")[1]
-		if (rs != "G") { rs = membersact[username].room; if (typeof zpravy[membersact[username].room] == "undefined") { zpravy[membersact[username].room] = [] } }
-		for (let i of zpravy[rs]) {
-			addChatMessage(i, false)
-		}
-	}
-}
-socket.on('new message', (data) => {
-	addChatMessage(data, true);
-}
-);
-const addChatMessage = (data, save) => {
-	rs = document.querySelector("#rs").className.split("switch")[1]
-	if (rs == data.skupina || (rs == "l" && typeof data.skupina == "number")) {
-		document.querySelector(".chat").innerHTML += '<p class="message" style="display: block;"><span class="username" style="color: #' + getUsernameColor(data.username) + ';">' + data.username + '</span><span class="messageBody">' + data.message + '</span></p>'
-	}
-	if (typeof zpravy[data.skupina] == "undefined") { zpravy[data.skupina] = [] }
-	if (save == true) { zpravy[data.skupina][zpravy[data.skupina].length] = data }
-}
-const sendMessage = () => {
-	var message = $("#mes").val();
-	// Prevent markup from being injected into the message
-	message = cleanInput(message);
-	// if there is a non-empty message and a socket connection
-	if (message) {
-		$("#mes").val('');
-		rs = document.querySelector("#rs").className.split("switch")[1]
-		if (rs == "g") {
-			addChatMessage({
-				username: username,
-				message: message,
-				skupina: "g"
-			}, true); socket.emit('new message', [message, "g"]);
-		} else {
-			addChatMessage({
-				username: username,
-				message: message,
-				skupina: membersact[username].room
-			}, true); socket.emit('new message', [message, membersact[username].room]);
-		}
-	}
-}
-$(window).keydown(event => {
-	if (event.which === 13) {
-		if (!username) {
-			$(".usernameInput").focus();
-			setUsername();// TODO: set username function
-		} else {
-			sendMessage()
-		}
-	}
-})
-const setUsername = () => {
-	username = cleanInput($(".usernameInput").val().trim());
-	// If the username is valid
-	if (username) {
-		// Tell the server your username
-		socket.emit('add user', username);
-		localStorage.setItem('name', username);
-		console.log(socket)
-	}
-}
-if (localStorage.getItem('name')) {
-	document.querySelector(".usernameInput").value = localStorage.getItem('name');// TODO: localStorage.setItem('name', username);
-}
-socket.on('', data => {
-
-})
 function removeElement(el) {
 	el.parentNode.removeChild(el);
 }
+function removeFromArray(arr, el) {
+	i = 0
+	ret = []
+	for (x of arr) {
+		if (x != el) {
+			ret[i] = x
+			i++
+		}
+	}
+}
+function removeFromObject(arr, el) {
+	i = 0
+	ret = []
+	for (x in arr) {
+		if (x != el) {
+			ret[i] = x
+			i++
+		}
+	}
+}
+const cleanInput = (input) => {
+	return $('<div/>').text(input).html();
+}
+const getUsernameColor = (username) => {
+	// Compute hash code
+	var hash = 7;
+	for (var i = 0; i < username.length; i++) {
+		hash = username.charCodeAt(i) + (hash << 5) - hash;
+	}
+	// Calculate color
+	var index = Math.abs(hash % COLORS.length);
+	return COLORS[index];
+}
+
+
+
+
+
+
+
+// MARK: game functions
+function creategamefield(us1, us2, size) {
+	el = document.createElement("table")
+	for (i = 0; i < size[1]+1; i++) {
+		el.appendChild(document.createElement("tr"))
+		for (y = 0; y < size[0]; y++) {
+			el.lastElementChild.appendChild(document.createElement("td"))
+		}
+	}
+	el.lastElementChild.className = "deployzone"
+	temp = document.querySelectorAll("#game>div>div")
+
+	temp2 = document.createElement("h1")
+	temp2.innerHTML = us1
+	el.className = us1
+	temp[0].appendChild(temp2)
+	temp[0].appendChild(document.createElement("br"))
+	temp[0].appendChild(el.cloneNode(true))
+
+	temp2 = document.createElement("h1")
+	temp2.innerHTML = us2
+	el.className = us2
+	temp[1].appendChild(temp2)
+	temp[1].appendChild(document.createElement("br"))
+	temp[1].appendChild(el);
+}
+
+
+
+
+
+
+
 // MARK: events
 function switchtologin() {
 	document.querySelector("#signup").style.display = "none"
@@ -107,7 +153,7 @@ async function signup() {
 		spam = true
 		Name = document.querySelectorAll("#signup input")[0].value.trim()
 		pass = document.querySelectorAll("#signup input")[1].value.trim()
-		const hash = await sha256(Name + pass)
+		const hash = await sha256(Name + pass + Name + pass + pass + Name + Name + pass)
 		socket.emit("signup", [Name, hash])
 		setTimeout(() => {
 			spam = false
@@ -128,16 +174,17 @@ async function login() {
 		spam = true
 		Name = document.querySelectorAll("#login input")[0].value.trim()
 		pass = document.querySelectorAll("#login input")[1].value.trim()
-		const hash = await sha256(Name + pass)
-		socket.emit("login", [Name, hash])
+		const hash = await sha256(Name + pass + Name + pass + pass + Name + Name + pass)
+		socket.emit("loginin", [Name, hash])
 		setTimeout(() => {
 			spam = false
 		}, 200);
 	}
 }
 socket.on('backlogin', data => {
-	console.log("login: " + data)
+	console.log("login: " + Boolean(data))
 	if (data) {
+		playerdata = data
 		removeElement(document.querySelector("#signup"))
 		removeElement(document.querySelector("#login"))
 		document.querySelector("main").style.display = "block"
@@ -163,8 +210,36 @@ document.querySelectorAll("#signup input").forEach(element => {
 })
 document.querySelector("aside input").addEventListener("keyup", function (event) {
 	if (event.keyCode === 13) {
-		// TODO: addmesage
+		sendMessage(cleanInput(event.target.value))
+		event.target.value = ""
 	}
 })
 
-// Mark: Comunication from server
+
+
+
+
+
+
+// MARK: Comunication with server
+socket.on('players', data => {
+	membersact = data;
+})
+socket.on('updataplayerdata', data => {
+	playerdata = data;
+})
+socket.on('serverlist', data => {
+	serverlist = data;
+	ret = "";
+	for (i in serverlist) {
+		ret += "<li><h3><b>" + serverlist[i].name + "</b></h3>" + serverlist[i].score + "<button onclick='socket.emit(\"joingame\",[\"" + serverlist[i].name + "\"])'>join</button></li>"
+	}
+	document.querySelector("#serverlist").innerHTML = ret
+})
+socket.on('placebase', data => {
+	ingame = 1
+	document.querySelector("#game").style.display = "block"
+	document.querySelector("#hry").style.display = "none"
+	document.querySelector(".ChatL").innerHTML = ""
+	creategamefield(data[0], data[1], data[2])
+})
